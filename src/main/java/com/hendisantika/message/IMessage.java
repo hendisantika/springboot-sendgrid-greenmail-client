748dabb86b32bb6c3f47eae0e1ea887a82120ba2
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:23:42 +0700

    #16 - Add IMessage

---
+++ src/main/java/com/hendisantika/message/IMessage.java
@@ -0,0 +1,27 @@
+package com.hendisantika.message;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/02/22
+ * Time: 19.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public interface IMessage {
+
+    String getMessage(String key);
+
+    MessageInfo getMessageInfo(String key);
+
+    List<MessageInfo> mergeMessages(final List<MessageInfo> messageList);
+
+    String getOutputMessage(HttpStatus status);
+}
