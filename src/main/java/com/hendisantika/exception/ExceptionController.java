59cb7a09327943129556d8389920398c7b4047c5
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:21:40 +0700

    #11 - Add ExceptionController

---
+++ src/main/java/com/hendisantika/exception/ExceptionController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/02/22
+ * Time: 19.21
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class ExceptionController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+}
