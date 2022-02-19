3e1d005feeaec3df25a204ece76ba4a64f7605dd
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:03:27 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootSendgridGreenmailClientApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSendgridGreenmailClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSendgridGreenmailClientApplication.class, args);
+    }
+
+}
