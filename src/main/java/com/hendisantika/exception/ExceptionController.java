cdaad6cd2a3c480593b69a8eb4f72ae1515d514f
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:22:08 +0700

    #12 - Add exceptionHandler in ExceptionController

--- src/main/java/com/hendisantika/exception/ExceptionController.java
+++ src/main/java/com/hendisantika/exception/ExceptionController.java
@@ -2,7 +2,9 @@ package com.hendisantika.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +20,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 public class ExceptionController {
 
     private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ExceptionHandler(InvalidEmailEntryException.class)
+    public ResponseEntity<OutputMessage> exceptionHandler(InvalidEmailEntryException ex) {
+        logger.error(ex.getMessage(), ex);
+        OutputMessage output = getStandardOutput(ex);
+        output.setErrorList(ex.getListErros());
+        return new ResponseEntity<>(output, ex.getStatus());
+    }
 
 }
