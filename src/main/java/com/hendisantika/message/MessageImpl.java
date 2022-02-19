d7f8175b3527763bcdaa693373ec6bd4c444b425
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:27:26 +0700

    #21 - Add get in MessageImpl

--- src/main/java/com/hendisantika/message/MessageImpl.java
+++ src/main/java/com/hendisantika/message/MessageImpl.java
@@ -2,7 +2,10 @@ package com.hendisantika.message;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +23,9 @@ public class MessageImpl implements IMessage {
     @Autowired
     private MessageSource messageSource;
 
+    private String get(String key) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(key, null, locale);
+    }
 
 }
