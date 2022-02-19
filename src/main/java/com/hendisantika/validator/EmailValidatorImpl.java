7e70649239abb02d2c202acfe1aac0a75d9af931
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-19 19:29:40 +0700

    #28 - Add validateEntry in EmailValidatorImpl

--- src/main/java/com/hendisantika/validator/EmailValidatorImpl.java
+++ src/main/java/com/hendisantika/validator/EmailValidatorImpl.java
@@ -1,8 +1,14 @@
 package com.hendisantika.validator;
 
+import com.hendisantika.dto.EmailRequestDTO;
+import com.hendisantika.exception.InvalidEmailEntryException;
 import com.hendisantika.message.IMessage;
+import com.hendisantika.message.MessageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,14 @@ public class EmailValidatorImpl implements EmailValidator {
 
     @Autowired
     private IMessage serviceMessage;
+
+    @Override
+    public void validateEntry(final EmailRequestDTO request) throws InvalidEmailEntryException {
+        List<MessageInfo> messages = new ArrayList<>();
+        validateRequiredFields(request, messages, this.serviceMessage);
+        if (!messages.isEmpty()) {
+            throw new InvalidEmailEntryException(serviceMessage.mergeMessages(messages),
+                    this.serviceMessage.getMessage("INVALID_ENTRIES_EMAIL"));
+        }
+    }
 }
