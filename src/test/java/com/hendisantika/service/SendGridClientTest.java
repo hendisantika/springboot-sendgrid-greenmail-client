05161fc778037e88ec61e16d4813a9ee4b8922f5
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 19:04:32 +0700

    #58 - Add testReceiveEmailWhenResponseIsEmpty in SendGridClientTest

--- src/test/java/com/hendisantika/service/SendGridClientTest.java
+++ src/test/java/com/hendisantika/service/SendGridClientTest.java
@@ -1,10 +1,22 @@
 package com.hendisantika.service;
 
 import com.hendisantika.client.SendGridClient;
+import com.hendisantika.dto.EmailResponseDTO;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +42,29 @@ public class SendGridClientTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         sendGridClient = new SendGridClient(sendGrid, "sendGridKey");
+    }
+
+    @Test
+    public void testReceiveEmailWhenResponseIsEmpty() throws Exception {
+        Response responseSendGrid = new Response();
+        when(sendGrid.api(Mockito.any(Request.class))).thenReturn(responseSendGrid);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        responseSendGrid.setHeaders(headers);
+        responseSendGrid.setStatusCode(STATUS_CODE);
+        responseSendGrid.setBody("");
+        EmailResponseDTO responseEmail = sendGridClient.send(createRequestEmail());
+        ArgumentCaptor<Request> requestCapture = ArgumentCaptor.forClass(Request.class);
+        verify(sendGrid, atLeastOnce()).api(requestCapture.capture());
+        Request request = requestCapture.getValue();
+        assertThat(responseEmail.getBody()).isEqualTo(responseSendGrid.getBody());
+        assertThat(responseEmail.getStatusCode()).isEqualTo(STATUS_CODE);
+        assertThat(responseEmail.getHeaders()).containsKey("Content-Type");
+        assertThat(request.getEndpoint()).isEqualTo("mail/send");
+        assertThat(request.getMethod().toString()).isEqualTo("POST");
+        assertThat(request.getBody()).isEqualTo("{\"from\":{\"email\":\"server@mail.com\"},"
+                + "\"subject\":\"E-mail confirmation\",\"personalizations\":[{\"to\":[{\"email\":\"user.test@mail.com\"}]}],"
+                + "\"content\":[{\"type\":\"text/html\",\"value\":\"Hello\"}],"
+                + "\"custom_args\":{\"customerAccountNumber\":\"sendGridKey\"}}");
     }
 }
