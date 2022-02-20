2a07b602c939b55afa008fce80775b2e7e3867aa
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 19:09:01 +0700

    #70 - Add FakeEmailServer

---
+++ src/test/java/com/hendisantika/service/fake/FakeEmailServer.java
@@ -0,0 +1,20 @@
+package com.hendisantika.service.fake;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/22
+ * Time: 19.08
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class FakeEmailServer {
+
+    private static final String MIME_TYPE_HTML = "text/html;";
+    private static final int TIMEOUT = 25000;
+}
