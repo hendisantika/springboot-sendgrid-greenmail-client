e65046f7613e1618a9381c117d7636bef134fdda
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 19:06:12 +0700

    #62 - Add TemplateServiceTest

---
+++ src/test/java/com/hendisantika/service/TemplateServiceTest.java
@@ -0,0 +1,34 @@
+package com.hendisantika.service;
+
+import com.samskivert.mustache.Template;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.autoconfigure.mustache.MustacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.mustache.MustacheResourceTemplateLoader;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/22
+ * Time: 19.05
+ * To change this template use File | Settings | File Templates.
+ */
+public class TemplateServiceTest {
+    @InjectMocks
+    private TemplateService service;
+
+    @Mock
+    private MustacheAutoConfiguration mustacheAutoConfiguration;
+
+    @Mock
+    private MustacheResourceTemplateLoader loader;
+
+    @Mock
+    private Compiler mustacheCompiler;
+
+    @Mock
+    private Template compile;
+}
