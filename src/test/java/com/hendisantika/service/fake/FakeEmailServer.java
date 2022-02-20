8628371c21485ea347caacdfb498913cc49764c7
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 19:09:17 +0700

    #71 - Add getInbox in FakeEmailServer

--- src/test/java/com/hendisantika/service/fake/FakeEmailServer.java
+++ src/test/java/com/hendisantika/service/fake/FakeEmailServer.java
@@ -1,6 +1,12 @@
 package com.hendisantika.service.fake;
 
+import com.hendisantika.base.EmailBaseIT;
 import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +23,11 @@ public class FakeEmailServer {
 
     private static final String MIME_TYPE_HTML = "text/html;";
     private static final int TIMEOUT = 25000;
+
+    public List<FakeMessage> getInbox() {
+        EmailBaseIT.getGreenMail().waitForIncomingEmail(TIMEOUT, 5);
+        MimeMessage[] emails = EmailBaseIT.getGreenMail().getReceivedMessages();
+        return Stream.of(emails).map(e -> getMessage(e))
+                .collect(Collectors.toList());
+    }
 }
