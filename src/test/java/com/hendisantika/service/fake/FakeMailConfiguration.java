4be4b3b934dd4aa9e4aea6e4d9cafe0dcd04cf68
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 19:10:28 +0700

    #74 - Add FakeMailConfiguration

---
+++ src/test/java/com/hendisantika/service/fake/FakeMailConfiguration.java
@@ -0,0 +1,18 @@
+package com.hendisantika.service.fake;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/22
+ * Time: 19.10
+ * To change this template use File | Settings | File Templates.
+ */
+
+@TestConfiguration
+public class FakeMailConfiguration {
+}
