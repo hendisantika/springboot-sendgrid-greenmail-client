c5d528f50172632b36c63c0f383cb55398912d09
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:01:42 +0700

    #30 - Add EmailService

---
+++ src/main/java/com/hendisantika/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.hendisantika.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-sendgrid-greenmail-client
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/22
+ * Time: 07.01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class EmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final EmailClientComponent emailClientComponent;
+    private final TemplateService templateService;
+
+    public EmailService(EmailClientComponent emailClientComponent
+            , EmailValidator emailValidatorImpl
+            , TemplateService templateService) {
+        this.emailClientComponent = emailClientComponent;
+        this.templateService = templateService;
+    }
+}
