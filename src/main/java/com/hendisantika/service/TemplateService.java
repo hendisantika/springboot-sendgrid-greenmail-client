8ee566424807964b911b51431a380155bb1bb003
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:03:45 +0700

    #34 - Add parseTemplateParams in TemplateService

--- src/main/java/com/hendisantika/service/TemplateService.java
+++ src/main/java/com/hendisantika/service/TemplateService.java
@@ -1,7 +1,15 @@
 package com.hendisantika.service;
 
+import com.hendisantika.dto.EmailRequestDTO;
+import com.hendisantika.exception.TemplateException;
+import com.samskivert.mustache.MustacheException;
+import lombok.NonNull;
 import org.springframework.boot.autoconfigure.mustache.MustacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.mustache.MustacheResourceTemplateLoader;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.Reader;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +29,22 @@ public class TemplateService {
 
     public TemplateService(MustacheAutoConfiguration mustacheAutoConfiguration) {
         this.mustacheAutoConfiguration = mustacheAutoConfiguration;
+    }
+
+    public @NonNull
+    String parseTemplateParams(EmailRequestDTO email) throws TemplateException {
+        try {
+            MustacheResourceTemplateLoader templateLoader = mustacheAutoConfiguration.mustacheTemplateLoader();
+            Reader template = templateLoader.getTemplate(email.getTemplateName());
+            return mustacheAutoConfiguration.mustacheCompiler(templateLoader)
+                    .compile(template)
+                    .execute(email.getTemplateParams());
+        } catch (FileNotFoundException e) {
+            throw new TemplateException("TEMPLATE_NOT_FOUND");
+        } catch (MustacheException e) {
+            throw new TemplateException("TEMPLATE_PARSE_ERROR", e);
+        } catch (Exception e) {
+            throw new TemplateException("GENERIC_ERROR", e);
+        }
     }
 }
