32cb3a5d5c069399204cb2ddeb8fa2e220092894
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 18:54:59 +0700

    #42 - Add send in SendGridClient

--- src/main/java/com/hendisantika/client/SendGridClient.java
+++ src/main/java/com/hendisantika/client/SendGridClient.java
@@ -1,9 +1,17 @@
 package com.hendisantika.client;
 
+import com.hendisantika.dto.EmailRequestDTO;
+import com.hendisantika.dto.EmailResponseDTO;
+import com.hendisantika.dto.RequestSendGridBuilder;
+import com.hendisantika.exception.EmailConnectionException;
 import com.hendisantika.service.EmailClientComponent;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +32,22 @@ public class SendGridClient implements EmailClientComponent {
     public SendGridClient(SendGrid sendGrid, @Value("${sendgrid.key}") String sendGridKey) {
         this.sendGrid = sendGrid;
         this.sendGridKey = sendGridKey;
+    }
+
+    @Override
+    public EmailResponseDTO send(EmailRequestDTO request) throws EmailConnectionException {
+        try {
+            Request sendGridRequest = RequestSendGridBuilder.of()
+                    .content(request.getContent())
+                    .subject(request.getSubject())
+                    .to(request.getTo())
+                    .from(request.getFrom())
+                    .key(sendGridKey)
+                    .build();
+            Response sendGridResponse = sendGrid.api(sendGridRequest);
+            return getResponse(sendGridResponse, sendGridRequest);
+        } catch (IOException e) {
+            throw new EmailConnectionException(e);
+        }
     }
 }
